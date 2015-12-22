c309191842ac85fc3f9f387a802dee5a098b72ec
TungLe <lethanhtung2908@gmail.com>
2015-12-22 11:51:26 +0700

    AddPermissonController

---
+++ HotelManagerApi/Controllers/BaseController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using HotelManagerApi.Models;
+
+namespace HotelManagerApi.Controllers
+{
+    public class BaseController : ApiController
+    {
+        public HotelEntitiesDataContext DB = new HotelEntitiesDataContext();
+    }
+}
---
+++ HotelManagerApi/Controllers/PermissionController.cs
@@ -0,0 +1,27 @@
+﻿using HotelManagerApi.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HotelManagerApi.Controllers
+{
+    public class PermissionController : BaseController
+    {
+        public ApiResponse GetAll()
+        {
+            //Khoang check permission
+
+            try
+            {
+                return ApiResponse.CreateSuccess(DB.Permissions.ToArray());
+            }
+            catch(Exception ex)
+            {
+                return ApiResponse.CreateFail(ex.Message);
+            }
+        }
+    }
+}
