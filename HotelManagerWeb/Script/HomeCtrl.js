9e7fde185365ca02dcec5f6b6cbb1cbfd8bacd96
thanhbebi17 <Truongphuc1710>
2015-12-30 11:38:51 +0700

    Add Login Page

---
+++ HotelManagerWeb/Script/HomeCtrl.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    'use strict';
+    app.controller('HomeCtrl', DemoCtrl);
+    function DemoCtrl($scope) {
+        var init = function () {
+            $scope.isRegister = false;
+            $scope.Account = {};
+            $scope.login = function () {
+                var k = 0;
+            };
+            $scope.register = function () {
+
+            };
+        };
+
+        init();
+    }
+})();
