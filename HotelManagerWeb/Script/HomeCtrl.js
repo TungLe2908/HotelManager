c0a7927173ec2e61aef7725800889fa2e05d073f
thanhbebi17 <Truongphuc1710>
2015-12-31 13:37:12 +0700

    Add OauthWeb

--- HotelManagerWeb/Script/HomeCtrl.js
+++ HotelManagerWeb/Script/HomeCtrl.js
@@ -1,12 +1,19 @@
 ﻿(function () {
     'use strict';
     app.controller('HomeCtrl', DemoCtrl);
-    function DemoCtrl($scope) {
+    function DemoCtrl($scope, $mdToast) {
         var init = function () {
             $scope.isRegister = false;
             $scope.Account = {};
             $scope.login = function () {
-                var k = 0;
+                if($scope.Account.Username && $scope.Account.Password)
+                {
+
+                }
+                else
+                {
+                    $mdToast.show($mdToast.simple().textContent('Please complete all fields'));
+                }
             };
             $scope.register = function () {
 
