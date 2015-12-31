6821167d6d9332fd13989813cd557b9d03a5aff7
thanhbebi17 <Truongphuc1710>
2015-12-31 14:12:28 +0700

    Add OauthWeb

---
+++ HotelOauth/Script/HomeCtrl.js
@@ -0,0 +1,25 @@
+﻿(function () {
+    'use strict';
+    app.controller('HomeCtrl', DemoCtrl);
+    function DemoCtrl($scope, $mdToast) {
+        var init = function () {
+            $scope.isRegister = false;
+            $scope.Account = {};
+            $scope.login = function () {
+                if($scope.Account.Username && $scope.Account.Password)
+                {
+
+                }
+                else
+                {
+                    $mdToast.show($mdToast.simple().textContent('Please complete all fields'));
+                }
+            };
+            $scope.register = function () {
+
+            };
+        };
+
+        init();
+    }
+})();
