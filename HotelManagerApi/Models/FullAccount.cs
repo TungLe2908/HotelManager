4b6f1a148bd942714cd6a9bd30a9588ac11c3bb2
TungLe <lethanhtung2908@gmail.com>
2015-12-31 10:32:32 +0700

    Add OauthProject

---
+++ HotelManagerApi/Models/FullAccount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HotelManagerApi.Models
+{
+    public class FullAccount
+    {
+        public string Email { set; get; }
+        public string Password { set; get; }
+        public string Phone { set; get; }
+        public string Name { set; get; }
+        public string Token { set; get; }
+        public int PermissionID { set; get; }
+    }
+}
