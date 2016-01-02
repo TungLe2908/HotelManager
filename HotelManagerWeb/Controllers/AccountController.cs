086f0ee63cbba8d8c21476c8ae1e5f9896af736b
thanhbebi17 <Truongphuc1710>
2016-01-02 19:33:55 +0700

    Done staff management

---
+++ HotelManagerWeb/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using HotelManagerWeb.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HotelManagerWeb.Controllers
+{
+    public class AccountController : BaseController
+    {
+        // GET: Account
+        [CheckPermission(new int[] { 2 })]
+        public ActionResult Staff()
+        {
+            return View();
+        }
+        [CheckPermission(new int[] { 2 })]
+        public ActionResult AddStaff(String Email, String Name)
+        {
+            using(HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("Token", Session.GetToken());
+                var Result = client.PostAsJsonAsync(GetConfig.Get("ApiUrl") + "account/AddStaffAccount", new { Email = Email, Name = Name }).Result;
+            }
+            return Redirect("/account/staff");
+        }
+
+        public ActionResult Index()
+        {
+            return Redirect("http://www.hoteloauth.somee.com/home/account?token="+Session.GetToken()+"&redir="+Request.UrlReferrer);
+        }
+    }
+}
--- HotelManagerWeb/Script/StaffCtrl.js
+++ HotelManagerWeb/Script/StaffCtrl.js
@@ -26,9 +26,25 @@
             {
                 location.href = 'http://hoteloauth.somee.com/home/register?redir=' + location.origin + "/account/addstaff";
             }
-            $scope.Delete =function(StaffID)
+            $scope.Delete =function(Email)
             {
-
+                var req = {
+                    method: 'POST',
+                    url: apiurl + "account/deletestaff",
+                    headers: {
+                        'token': token
+                    },
+                    data: '"' + Email + '"'
+                }
+                $http(req).then(function (Result) {
+                    if (Result.data.Code == 1) {
+                        $rootScope.showMessage('Delete success');
+                        $scope.Load();
+                    }
+                    else {
+                        $rootScope.showMessage('Something error');
+                    }
+                });
             }
         }
         init();
